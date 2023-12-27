b831c0f8b5b46fb563b2133fc76131b9aabed4ba
yushulx <lingxiao1002@gmail.com>
2023-12-27 06:56:19 +0000

    Deploying to gh-pages from  @ e0c6ea466f6b26be10f764b9d3d78f5a1a33dce4 🚀

--- _content/RazorDocumentLibrary/documentJsInterop.js
+++ _content/RazorDocumentLibrary/documentJsInterop.js
@@ -59,11 +59,14 @@ export async function detectCanvas(normalizer, canvas) {
     return null;
 }
 
+let globalCanvas;
+let globalContext;
 export async function rectifyCanvas(normalizer, canvas, location) {
     if (!Dynamsoft) return;
 
     try {
-        let result = await normalizer.normalize(canvas, { quad: JSON.parse(location) });
+        let points = JSON.parse(location);
+        let result = await normalizer.normalize(canvas, { quad: points });
         if (result.image) {
             return result.image.toCanvas();
         }
@@ -77,16 +80,46 @@ export async function rectifyCanvas(normalizer, canvas, location) {
     return null;
 }
 
-export async function showRectifiedDocument(elementId, canvas)
-{
+export async function showDocumentEditor(dotNetHelper, cbName, elementId, canvas, location) {
+    if (!Dynamsoft) return;
+    try {
+        let parent = document.getElementById(elementId);
+        parent.innerHTML = '';
+
+        parent.appendChild(canvas);
+
+        // Create a new canvas element
+        globalCanvas = document.createElement('canvas');
+        globalCanvas.id = 'overlayCanvas';
+        globalCanvas.width = canvas.width; // Match the width of the base canvas
+        globalCanvas.height = canvas.height; // Match the height of the base canvas
+
+        // Set positioning to overlay the base canvas
+        globalCanvas.style.position = 'absolute';
+        globalCanvas.style.left = canvas.offsetLeft + 'px';
+        globalCanvas.style.top = canvas.offsetTop + 'px';
+
+        // Add the overlay canvas to the parent of the base canvas
+        parent.appendChild(globalCanvas);
+
+        //globalCanvas = canvas;
+        globalContext = globalCanvas.getContext("2d");
+        let data = JSON.parse(location);
+        globalCanvas.addEventListener("mousedown", (event) => updatePoint(event, dotNetHelper, cbName, data.points));
+        globalCanvas.addEventListener("touchstart", (event) => updatePoint(event, dotNetHelper, cbName, data.points));
+        drawQuad(dotNetHelper, cbName, data.points);
+    }
+    catch (ex) {
+        console.error(ex);
+    }
+    return null;
+}
+export async function showRectifiedDocument(elementId, canvas) {
     if (!Dynamsoft) return;
 
     try {
         let parent = document.getElementById(elementId);
-
-        if (parent.firstChild) {
-            parent.removeChild(parent.firstChild);
-        }
+        parent.innerHTML = '';
         parent.appendChild(canvas);
     }
     catch (ex) {
@@ -95,8 +128,7 @@ export async function showRectifiedDocument(elementId, canvas)
     return null;
 }
 
-export async function setFilter(normalizer, filter)
-{
+export async function setFilter(normalizer, filter) {
     if (!Dynamsoft) return;
 
     try {
@@ -111,3 +143,54 @@ export async function setFilter(normalizer, filter)
     return null;
 }
 
+function updatePoint(e, dotNetHelper, cbName, points) {
+    let rect = globalCanvas.getBoundingClientRect();
+
+    let scaleX = globalCanvas.clientWidth / globalCanvas.width;
+    let scaleY = globalCanvas.clientHeight / globalCanvas.height;
+    let mouseX = (e.clientX - rect.left) / scaleX;
+    let mouseY = (e.clientY - rect.top) / scaleY;
+
+    let delta = 10;
+    for (let i = 0; i < points.length; i++) {
+        if (Math.abs(points[i].x - mouseX) < delta && Math.abs(points[i].y - mouseY) < delta) {
+            globalCanvas.addEventListener("mousemove", dragPoint);
+            globalCanvas.addEventListener("mouseup", releasePoint);
+            globalCanvas.addEventListener("touchmove", dragPoint);
+            globalCanvas.addEventListener("touchend", releasePoint);
+            function dragPoint(e) {
+                let rect = globalCanvas.getBoundingClientRect();
+                let mouseX = e.clientX || e.touches[0].clientX;
+                let mouseY = e.clientY || e.touches[0].clientY;
+                points[i].x = Math.round((mouseX - rect.left) / scaleX);
+                points[i].y = Math.round((mouseY - rect.top) / scaleY);
+                drawQuad(dotNetHelper, cbName, points);
+            }
+            function releasePoint() {
+                globalCanvas.removeEventListener("mousemove", dragPoint);
+                globalCanvas.removeEventListener("mouseup", releasePoint);
+                globalCanvas.removeEventListener("touchmove", dragPoint);
+                globalCanvas.removeEventListener("touchend", releasePoint);
+            }
+            break;
+        }
+    }
+}
+
+function drawQuad(dotNetHelper, cbName, points) {
+    globalContext.clearRect(0, 0, globalCanvas.width, globalCanvas.height);
+    globalContext.strokeStyle = "red";
+    for (let i = 0; i < points.length; i++) {
+        globalContext.beginPath();
+        globalContext.arc(points[i].x, points[i].y, 5, 0, 2 * Math.PI);
+        globalContext.stroke();
+    }
+    globalContext.beginPath();
+    globalContext.moveTo(points[0].x, points[0].y);
+    globalContext.lineTo(points[1].x, points[1].y);
+    globalContext.lineTo(points[2].x, points[2].y);
+    globalContext.lineTo(points[3].x, points[3].y);
+    globalContext.lineTo(points[0].x, points[0].y);
+    globalContext.stroke();
+    dotNetHelper.invokeMethodAsync(cbName, { "location": { "points": points } });
+}
